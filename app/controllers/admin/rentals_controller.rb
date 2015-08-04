ab7489671dde383640bb0b42273cd1cab1a2394d
Andrew Glass <andrewglass1@gmail.com>
2015-08-04 14:19:18 -0400

    new admin rental retreival

--- app/controllers/admin/rentals_controller.rb
+++ app/controllers/admin/rentals_controller.rb
@@ -12,6 +12,17 @@ class Admin::RentalsController < ApplicationController
   end
 
   def search
+  end
 
+  def find
+    @rental = Rental.where(current: true,
+                           last_name: params["last_name"],
+                           phone_number: params["phone_number"]).first
+    if @rental
+      redirect_to admin_rental_path(@rental)
+    else
+      flash[:notice] = "Sorry, we couldn't find a current rental with that information.  Please try again."
+      redirect_to search_admin_rentals_path
+    end
   end
 end
