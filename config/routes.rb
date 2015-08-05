37f1a978116afe6304ee24d726093a1c83159d80
Andrew Glass <andrewglass1@gmail.com>
2015-08-05 14:56:07 -0400

    clean routes file

--- config/routes.rb
+++ config/routes.rb
@@ -3,29 +3,26 @@ Rails.application.routes.draw do
   root 'rentals#hub', :device_id => "1"
 
   scope ":device_id" do
-  # namespace '/devices/:device_id' do
-    # scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/, defaults: {locale: "en"} do
-
-  resources :rentals, only: [:new, :create, :show] do
-    collection do
-      get 'hub'
-      get 'retrieve'
-      put 'complete'
-    end
-  end
-
-  namespace :admin do
-    resources :lockers, only: [:index, :show]
-    resources :rentals, only: [:show] do
-      member do
-        post 'retrieve'
-      end
+    resources :rentals, only: [:new, :create, :show] do
       collection do
-        get 'search'
-        post 'find'
+        get 'hub'
+        get 'retrieve'
+        put 'complete'
       end
     end
-  end
-  match 'admin' => "admin/lockers#index", via: 'get'
+
+    namespace :admin do
+      resources :lockers, only: [:index, :show]
+      resources :rentals, only: [:show] do
+        member do
+          post 'retrieve'
+        end
+        collection do
+          get 'search'
+          post 'find'
+        end
+      end
+    end
+    match 'admin' => "admin/lockers#index", via: 'get'
   end
 end
