eae38cbdf5a22741859a6450004b6cf8f3d6eb9a
Andrew Glass <andrewglass1@gmail.com>
2015-08-06 12:31:35 -0400

    skip before filters in testingscontroller

--- app/controllers/testings_controller.rb
+++ app/controllers/testings_controller.rb
@@ -1,5 +1,7 @@
 class TestingsController < ApplicationController
   protect_from_forgery
+  skip_before_filter :validate_device_id
+  skip_before_filter :basic_auth
 
   def good
     render :json => { }, :status => 200
