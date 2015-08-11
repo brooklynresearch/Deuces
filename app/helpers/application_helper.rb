edad9224d301bdd0e4bbab0175069756ae0bf769
Andrew Glass <andrewglass1@gmail.com>
2015-08-11 18:11:30 -0400

    render page title dynamically

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def page_title
+    if [Admin::LockersController, Admin::RentalsController].include?(controller.class)
+      "Admin"
+    else
+      "PowerUp"
+    end
+  end
 end
