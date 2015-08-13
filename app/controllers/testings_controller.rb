44518e3bdec3b79d8370f46b45ea8caa5bd55f9e
Giblets <johnny@brooklynresearch.org>
2015-08-13 15:57:30 -0400

    remove more basic auth

--- app/controllers/testings_controller.rb
+++ app/controllers/testings_controller.rb
@@ -1,7 +1,6 @@
 class TestingsController < ApplicationController
   protect_from_forgery
   skip_before_filter :validate_device_id
-  skip_before_filter :basic_auth
 
   def good
     render :json => { }, :status => 200
