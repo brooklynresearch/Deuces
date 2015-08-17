49e4028a4be5f0bbe5886a73c253511aa82684ca
Giblets <johnny@brooklynresearch.org>
2015-08-17 19:10:53 -0400

    new keep open script

--- lib/bash_scripts/keep_open.sh
+++ lib/bash_scripts/keep_open.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/bash
-# A script to keep alive rails server
-
+#!/bin/sh
+PATH=/Users/stillness/.rvm/gems/ruby-2.2.1/bin:/Users/stillness/.rvm/gems/ruby-2.2.1@global/bin:/Users/stillness/.rvm/rubies/ruby-2.2.1/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Users/stillness/.rvm/bin
+source ~/.bash_profile
 if [ $(netstat -an | grep 3000 | grep LISTEN | wc -l) -eq 0 ]
 then
   cd ~/Documents/Deuces/
@@ -12,5 +12,6 @@ then
     echo $i;
     sleep 1s
   done
+  echo 'bout to start rs'
   rails server -b 0.0.0.0 -e production -d
 fi
--- lib/bash_scripts/start_deuces.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/bash
-# A script to start the rails server
-cd ~/Documents/Deuces/
-rails server -b 0.0.0.0 -e production -d
-
