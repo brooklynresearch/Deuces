b711578f57c90e8646d900aaf37055369f4fdf4a
Andrew Glass <andrewglass1@gmail.com>
2015-08-19 18:53:00 -0400

    seeds file reflects large locker issue

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,24 @@
 row_count = 9
 column_count = 17
 
+rows_with_large = [0,2,4,6]
 
-large_lockers = [[0,1], [0,2],[0,3],[0,4][0,5][0,6]]
 
 row_count.times do |r|
-  column_count.times do |c|
-    large = large_lockers.include?([r,c])
-    Locker.create(row: r, column: c, large: large)
+  if rows_with_large.include?(r)
+    #create large locker- in first column
+    Locker.create(row: r, column: 0, large: true)
+    #no locker in second column
+    #
+    #create 3rd - n column lockers for row
+    (2...column_count).each do |c|
+      Locker.create(row: r, column: c, large: false)
+    end
+  else
+
+    column_count.times do |c|
+      Locker.create(row: r, column: c, large: false)
+    end
+
   end
 end
