bb61111e69c5e031b5cfcc91a896524f765a574b
Giblets <johnny@brooklynresearch.org>
2015-08-19 17:25:34 -0400

    remove extraneous code in keep_alive script

--- lib/bash_scripts/keep_open.sh
+++ lib/bash_scripts/keep_open.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-PATH=/Users/stillness/.rvm/gems/ruby-2.2.1/bin:/Users/stillness/.rvm/gems/ruby-2.2.1@global/bin:/Users/stillness/.rvm/rubies/ruby-2.2.1/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Users/stillness/.rvm/bin
-source ~/.bash_profile
 if [ $(netstat -an | grep 3000 | grep LISTEN | wc -l) -eq 0 ]
 then
   cd ~/Documents/Deuces/
