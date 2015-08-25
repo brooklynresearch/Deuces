34195b8fb07da9e87bc0461d66322ba71e93d44c
Andrew Glass <andrewglass1@gmail.com>
2015-08-25 11:44:25 -0400

    fix admin search routing

--- app/controllers/admin/rentals_controller.rb
+++ app/controllers/admin/rentals_controller.rb
@@ -2,6 +2,7 @@ class Admin::RentalsController < ApplicationController
   before_action :admin_basic_auth
 
   def show
+    set_name_and_phone_search
     @rental = Rental.find(params[:id])
   end
 
@@ -19,8 +20,7 @@ class Admin::RentalsController < ApplicationController
   end
 
   def search
-    @last_name = params["last_name"] || ""
-    @phone_number = params["phone_number"] || ""
+    set_name_and_phone_search
   end
 
   def confirm
@@ -35,7 +35,7 @@ class Admin::RentalsController < ApplicationController
     @rentals = Rental.find_all_current(params["last_name"], params["phone_number"])
 
     if @rentals.length == 1
-      redirect_to admin_rental_path(@rentals.first, search: "t")
+      redirect_to admin_rental_path(@rentals.first, search: "t", last_name: params["last_name"], phone_number: params["phone_number"] )
     elsif @rentals.length > 1
       redirect_to results_admin_rentals_path(rental_ids: @rentals.map(&:id), last_name: params["last_name"], phone_number: params["phone_number"] )
     else
@@ -45,8 +45,14 @@ class Admin::RentalsController < ApplicationController
   end
 
   def results
-    @last_name = params["last_name"].upcase
-    @phone_number = params["phone_number"]
+    set_name_and_phone_search
     @rentals = Rental.find(params["rental_ids"])
+  end
+
+
+  def set_name_and_phone_search
+    @last_name = params["last_name"] || ""
+    @phone_number = params["phone_number"] || ""
+
   end
 end
