c1c06c9fb09756cfb688c0dad9f471fd4ab0640e
Giblets <johnny@brooklynresearch.org>
2015-09-04 11:00:00 -0400

    remove db restart bash

--- lib/bash_scripts/restart_db.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-rm /usr/local/var/postgres/postmaster.pid
-sleep 5s
-launchctl unload ~/Library/LaunchAgents/homebrew.mxcl.postgresql.plist
-sleep 5s
-launchctl load ~/Library/LaunchAgents/homebrew.mxcl.postgresql.plist
